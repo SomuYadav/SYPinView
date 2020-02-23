1b9d46e88a6add428c4784fbd1d0b24a8ce76eb1
Somendra Yadav <rajesh.yadav@consagous.com>
2020-02-23 22:52:52 +0530

    Initial Commit

---
+++ SYPinView/SYPinView.h
@@ -0,0 +1,19 @@
+//
+//  SYPinView.h
+//  SYPinView
+//
+//  Created by MacHD on 23/02/20.
+//  Copyright © 2020 Somendra. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SYPinView.
+FOUNDATION_EXPORT double SYPinViewVersionNumber;
+
+//! Project version string for SYPinView.
+FOUNDATION_EXPORT const unsigned char SYPinViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SYPinView/PublicHeader.h>
+
+
